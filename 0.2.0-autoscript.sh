bae4f5fe1683a72c1efc99748f5c9a6913b91a06
hazennetworksolutions <161881320+hazennetworksolutions@users.noreply.github.com>
2024-11-22 16:56:26 +0300

    Update 0.2.0-autoscript.sh

--- 0.2.0-autoscript.sh
+++ 0.2.0-autoscript.sh
@@ -180,10 +180,10 @@ sudo systemctl enable junctiond
 
 # Initialize the node
 printGreen "7. Initializing the node..."
-prysmd config set client chain-id ${AIRCHAINS_CHAIN_ID}
-prysmd config set client keyring-backend test
-prysmd config set client node tcp://localhost:${AIRCHAINS_PORT}657
-prysmd init ${MONIKER} --chain-id ${AIRCHAINS_CHAIN_ID}
+junctiond config set client chain-id ${AIRCHAINS_CHAIN_ID}
+junctiond config set client keyring-backend test
+junctiond config set client node tcp://localhost:${AIRCHAINS_PORT}657
+junctiond init ${MONIKER} --chain-id ${AIRCHAINS_CHAIN_ID}
 
 # Download genesis and addrbook files
 printGreen "8. Downloading genesis and addrbook..."
